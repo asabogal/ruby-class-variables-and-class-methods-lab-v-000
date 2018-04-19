8e7473df6ce06257a132cdfc86fd8bc8ea0ae031
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-19 17:59:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,18 @@
 class Song
 
-  attr_accessor :name, :artist, :genre
-
   @@count = 0
   @@artists = []
 
-  def self.count
-    @@song_count
-  end
+
+  attr_accessor :name, :artist, :genre
 
   def initialize(name, artist, genre)
     @@count += 1
   end
 
+  def self.count
+    @@song_count
+  end
 
 
 end
